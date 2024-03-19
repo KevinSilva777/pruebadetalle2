d428e34f82e390c040994dc5b59a133fa39827e9
KevinDavidSilvaBeltran <sb24002@esfe.agape.edu.sv>
2024-03-18 21:05:47 -0600

    Agregar archivos de proyecto.

---
+++ pruebadetalle2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pruebadetalle2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
